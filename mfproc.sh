1b9a84b035ee22a674a6878f0f1f4b3cd8c04431
zeus <vasilismanolakas@gmail.com>
2025-12-28 14:09:11 -0500

    proc

--- mfproc.sh
+++ mfproc.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 printf "\n"
 printf "Script 3: mfproc\n"
-printf "Max args : 2\n"
-if (( $# > 2 )); then
-    printf "Maximum allowable parameters are 2. You entered $#. Exiting...\n"
+printf "Max args : 3\n"
+if (( $# > 3 )); then
+    printf "Maximum allowable parameters are 3. Exiting...\n"
     exit 1
 fi
+
 printf "Args given: $#\n"
 printf "Usage: mfproc [-u username] [-s S|R|Z]"
 printf "\n"
+
 # 0 param or 1 param and invalid user
-if [[ $# == 0 || ( $# == 1 && ! $(id "$1" >/dev/null 2>&1; echo $?) -eq 0 ) ]]; then
-    echo "No parameters given, or, the user '$1' is invalid. Instead, all active processes of the operating system will be listed."
+if [[ $# == 0 || ( $# == 2 && ! $(id "$2" >/dev/null 2>&1; echo $?) -eq 0 ) ]]; then
+    echo "No parameters given, or, the user '$2' is invalid. Instead, all active processes of the operating system will be listed."
     printf "\n"
     cd /proc
     find /proc -maxdepth 1 -type d -name '[0-9]*' > /dev/null 2>&1  #this finds all processes running on the OS.
@@ -29,13 +31,12 @@ for directory in /proc/[0-9]*; do
     "$name" "$pid" "$ppid" "$uid" "$gid" "$state" "$write_locked_files" "$read_locked_files"
 done
     exit 0
-echo "User '$1' does not exist. Instead, all active processes of the operating system will be listed."
-  exit 1
+  return 1
 # 1 param & valid user
 # If the user inputs a valid username: , the input will be ./mfproc.sh -u USERNAME. Therefore $1 = "u" & $2 = "USERNAME."So:
 elif  (( $# == 2 ))  &&  [[ "$1" == "-u" ]] &&  id "$2" >/dev/null 2>&1 ; then
-printf "1 parameter entered, and the user is valid."
-printf "Now we need to print each process the specific user is running.\n\n"
+printf "1 parameter entered, -u entered, and the user '$2' is valid and exists.\n"
+printf "Now we need to print each process that '$2' running.\n\n"
 user_id=$( id -u "$2" ) # now i have the specific user's ID
 #  now i need to iterate over each process, and check for this UID. if the UIDs match,
 #  i print the process. Sounds Simple.
