8c85a23da08588872d083a1e0a95f85bf5c061af
zeus <vasilismanolakas@gmail.com>
2026-01-04 08:34:40 -0500

    mfproc 100% complete

--- mfproc.sh
+++ mfproc.sh
@@ -4,15 +4,18 @@ printf "Script 3: mfproc\n"
 printf "Max args : 4\n"
 if (( $# > 4 )); then
     printf "Maximum allowable parameters are 4. Exiting...\n"
-    exit 1
+    exit
 fi
-
-printf "Args given: $#\n"
-printf "Usage: mfproc [-u username] [-s S|R|Z]"
+# 4 Cases:
+#0 params -> print every process
+#1 param -> could be either username or process state .
+#2 params -> both username and states given.
+printf "Number of args given: $#\n"
+printf "Use as: mfproc [-u username] [-s S|R|Z]"
 printf "\n"
 
 # 0 param or 1 param and invalid user
-if [[ $# == 0 || ( $# == 2 && ! $(id "$2" >/dev/null 2>&1; echo $?) -eq 0 ) ]]; then
+if [[ $# == 0 || ( $# == 2 && ! $(id "$2" >/dev/null 2>&1; ) ) ]]; then
     echo "No parameters given, or, the user '$2' is invalid. Instead, all active processes of the operating system will be listed."
     printf "\n"
     cd /proc
@@ -30,7 +33,11 @@ for directory in /proc/[0-9]*; do
     printf "%-35.35s %7s %7s %7s %10s %-20.20s %6s %6s\n" \
     "$name" "$pid" "$ppid" "$uid" "$gid" "$state" "$write_locked_files" "$read_locked_files"
 done
-    exit 0
+if (( $# == 2 ))  && !  id "$2" >/dev/null 2>&1; then
+  exit 1  # Non existent user
+elif (( $# == 0 )); then
+  exit 0
+fi
   return 1
 # 1 param & valid user
 # If the user inputs a valid username: , the input will be ./mfproc.sh -u USERNAME. Therefore $1 = "u" & $2 = "USERNAME."So:
@@ -56,7 +63,7 @@ state=$(grep -m1 '^State:' "$status" 2>/dev/null | cut -f2-)
 fi
 done
 elif (( $# == 4 )) && [[ "$1" == "-u" ]] && [[ "$3" == "-s" ]] && id "$2" >/dev/null 2>&1; then
-  # Correct input: -u USER -s S
+  # Correct input: -u USER -s S|R|Z
   uuid=$(id -u "$2")  # user's UID
 
   for directory in /proc/[0-9]*; do
